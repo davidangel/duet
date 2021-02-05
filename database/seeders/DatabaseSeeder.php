d0f0c8e5809fa7d883ef6b31972ae5e52faa4bf1
David Angel <david@davidangel.net>
2021-02-04 20:41:01 -0500

    Add Database Seeder for dev user.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'David',
+            'email' => 'david@davidangel.net',
+            'password' => Hash::make('password'),
+        ]);
+
+        DB::table('teams')->insert([
+            'name' => 'David\'s Team',
+            'user_id' => 1,
+            'personal_team' => 1,
+        ]);
     }
 }
