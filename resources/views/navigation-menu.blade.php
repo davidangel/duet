16938f0d49c12842f6eaac9dcda17428a7ad02e2
David Angel <david@davidangel.net>
2021-02-14 14:08:27 -0500

    Update nav for better mobile experience.

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,6 @@
 
         <!-- Navigation Links -->
         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-          {{-- <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-jet-nav-link> --}}
           @foreach (Auth::user()->allTeams() as $team)
 
             <form method="POST" action="{{ route('current-team.update') }}">
@@ -34,7 +31,7 @@
         </div>
       </div>
 
-      <div class="hidden sm:flex sm:items-center sm:ml-6">
+      <div class="flex items-center sm:ml-6">
         <!-- Teams Dropdown -->
         @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
           <div class="relative ml-3">
@@ -150,7 +147,7 @@
       </div>
 
       <!-- Hamburger -->
-      <div class="flex items-center -mr-2 sm:hidden">
+      {{-- <div class="flex items-center -mr-2 sm:hidden">
         <button @click="open = ! open"
           class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
           <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -160,89 +157,12 @@
               stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
-      </div>
+      </div> --}}
     </div>
   </div>
 
   <!-- Responsive Navigation Menu -->
-  <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-    <div class="pt-2 pb-3 space-y-1">
-      <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-        {{ __('Dashboard') }}
-      </x-jet-responsive-nav-link>
-    </div>
-
-    <!-- Responsive Settings Options -->
-    <div class="pt-4 pb-1 border-t border-gray-200">
-      <div class="flex items-center px-4">
-        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-          <div class="flex-shrink-0 mr-3">
-            <img class="object-cover w-10 h-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}"
-              alt="{{ Auth::user()->name }}" />
-          </div>
-        @endif
-
-        <div>
-          <div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
-          <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
-        </div>
-      </div>
-
-      <div class="mt-3 space-y-1">
-        <!-- Account Management -->
-        <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-          {{ __('Profile') }}
-        </x-jet-responsive-nav-link>
-
-        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-          <x-jet-responsive-nav-link href="{{ route('api-tokens.index') }}"
-            :active="request()->routeIs('api-tokens.index')">
-            {{ __('API Tokens') }}
-          </x-jet-responsive-nav-link>
-        @endif
-
-        <!-- Authentication -->
-        <form method="POST" action="{{ route('logout') }}">
-          @csrf
-
-          <x-jet-responsive-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-            {{ __('Logout') }}
-          </x-jet-responsive-nav-link>
-        </form>
-
-        <!-- Team Management -->
-        @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
-          <div class="border-t border-gray-200"></div>
-
-          <div class="block px-4 py-2 text-xs text-gray-400">
-            {{ __('Manage Team') }}
-          </div>
-
-          <!-- Team Settings -->
-          <x-jet-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
-            :active="request()->routeIs('teams.show')">
-            {{ __('Team Settings') }}
-          </x-jet-responsive-nav-link>
-
-          @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-            <x-jet-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-              {{ __('Create New Team') }}
-            </x-jet-responsive-nav-link>
-          @endcan
-
-          <div class="border-t border-gray-200"></div>
-
-          <!-- Team Switcher -->
-          <div class="block px-4 py-2 text-xs text-gray-400">
-            {{ __('Switch Teams') }}
-          </div>
-
-          @foreach (Auth::user()->allTeams() as $team)
-            <x-jet-switchable-team :team="$team" component="jet-responsive-nav-link" />
-          @endforeach
-        @endif
-      </div>
-    </div>
-  </div>
+  {{-- <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    unused hamburger menu
+  </div> --}}
 </nav>
